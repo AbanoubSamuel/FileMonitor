98f2bc7ea666c6ad5b0a31b6ff4f53693f2193a2
Abanoub Samuel <abanoub.samuel@hotmail.com>
2024-03-20 12:41:18 +0200

    first commit

---
+++ src/main/java/org/abg/filemonitor/FileMonitor.java
@@ -0,0 +1,13 @@
+package org.abg.filemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileMonitor {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileMonitor.class, args);
+    }
+
+}
---
+++ src/test/java/org/abg/filemonitor/FileMonitorTests.java
@@ -0,0 +1,13 @@
+package org.abg.filemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileMonitorTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
